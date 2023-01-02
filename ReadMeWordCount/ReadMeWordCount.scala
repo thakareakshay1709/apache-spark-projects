c5268718b304228298f195b6064395f20e748227
Akshay Thakare <akshay.thakare@bpi-unitek.com>
2023-01-02 20:11:17 +0000

    adding basic scala program to count words from readme file

---
+++ ReadMeWordCount/ReadMeWordCount.scala
@@ -0,0 +1,11 @@
+val textFile = sc.textFile("D:/Programming/Scala/apache-spark-projects/README.md")
+
+val tokenizedFileData = textFile.flatMap(line=>line.split(" "))
+
+val countPrep = tokenizedFileData.map(word=>(word, 1))
+
+val sortedCounts = counts.sortBy(KyPair=>kyPair._2, false)
+
+val sortedCounts = counts.sortBy(kyPair=>kyPair._2, false)
+
+sortedCounts.saveAsTextFile("D:/Programming/Scala/apache-spark-projects/ReadMeWordCount")
