c0921bbd854b2561054c9e2cb5e1268ba1c19886
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2023-01-06 23:19:23 +0000

    modifying readmewordcount script

--- ReadMeWordCount/ReadMeWordCount.scala
+++ ReadMeWordCount/ReadMeWordCount.scala
@@ -8,6 +8,6 @@ val countPrep = tokenizedFileData.map(word=>(word, 1))
 
 val sortedCounts = counts.sortBy(KyPair=>kyPair._2, false)
 
-val sortedCounts = counts.sortBy(kyPair=>kyPair._2, false)
+sortedCounts.collect
 
 sortedCounts.saveAsTextFile("D:/Programming/Scala/apache-spark-projects/ReadMeWordCount")
